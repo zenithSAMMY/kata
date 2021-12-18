dd5a8e8ce264e454088d7094ae1a757a978c9264
zenithSAMMY <z2006005@gmail.com>
2021-12-18 22:22:04 +0800

    Add files via upload

---
+++ Find_the_divisors.cs
@@ -0,0 +1,51 @@
+﻿// https://www.codewars.com/kata/544aed4c4a30184e960010f4/train/csharp
+
+using System;
+using System.Collections.Generic;
+
+namespace Find_the_divisors
+{
+    public class Kata
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] Divisors(int n)
+        {
+            List<int> numsList = new List<int>();
+            for (var i = 2; i <= Math.Round(Math.Sqrt(n), 2); i++)
+            {
+                if (n % i == 0)
+                {
+                    numsList.Add(i);
+                    Console.WriteLine(i);
+                    if (n / i == i)
+                    {
+
+                    }
+                    else
+                    {
+                        numsList.Add(n / i);
+                        Console.WriteLine(n / i);
+                    }
+                }
+            }
+            Console.WriteLine(numsList.Count);
+            if (numsList.Count == 0)
+            {
+                return null;
+            }
+            numsList.Sort();
+            int [] nums = numsList.ToArray();
+            Console.WriteLine(nums);
+            foreach (var VARIABLE in nums)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+            return nums;
+        }
+
+
+    }
+}
---
+++ Find_the_divisors_Tests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Find_the_divisors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_divisors.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void DivisorsTest()
+        {
+            // zero divisor
+            Assert.AreEqual(null, Kata.Divisors(13));
+            // one divisor
+            CollectionAssert.AreEqual(new int[] { 5 }, Kata.Divisors(25));
+            // // // two prime numbers divisors
+            CollectionAssert.AreEqual(new int[] { 11, 23 }, Kata.Divisors(253));
+            // // many divisors
+            CollectionAssert.AreEqual(new int[] { 2, 3, 4, 6, 8, 12 }, Kata.Divisors(24));
+        }
+    }
+}
