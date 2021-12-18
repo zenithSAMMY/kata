a82c9c5e955cad38775f012527198c06fefff61c
zenithSAMMY <z2006005@gmail.com>
2021-12-18 22:21:13 +0800

    Add files via upload

---
+++ Ones_and_Zeros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Solution
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class  Kata
+    {
+        public int BinaryArrayToNumber(int[] BinaryArray)
+        {
+            //Code here
+            int arr_len = BinaryArray.Length;
+            double res = 0;
+            int power = arr_len - 1;
+            for (int i = 0; i < arr_len; i++)
+            {
+                res += BinaryArray[i] * Math.Pow(2, power);
+                power--;
+            }
+            return Convert.ToInt32(res);
+        }
+    }
+}
---
+++ Ones_and_Zeros_test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    [TestClass()]
+    public class KataTests
+    {
+        int[] Test1 = new int[] { 0, 0, 0, 0 };
+        int[] Test2 = new int[] { 1, 1, 1, 1 };
+        int[] Test3 = new int[] { 0, 1, 1, 0 };
+        int[] Test4 = new int[] { 0, 1, 0, 1 };
+        private Kata kata = new Kata();
+        [TestMethod()]
+        public void BinaryArrayToNumberTest()
+        {
+            Assert.AreEqual(0, kata.BinaryArrayToNumber(Test1));
+            Assert.AreEqual(15, kata.BinaryArrayToNumber(Test2));
+            Assert.AreEqual(6, kata.BinaryArrayToNumber(Test3));
+            Assert.AreEqual(5, kata.BinaryArrayToNumber(Test4));
+        }
+    }
+}
