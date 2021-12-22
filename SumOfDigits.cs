9b402fc6a0996cc847bfb816d3b349a3ed0cbe13
zenithSAMMY <z2006005@gmail.com>
2021-12-22 16:12:48 +0800

    Add files via upload

---
+++ SumOfDigits.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Sum_of_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Number
+    {
+        public int DigitalRoot(long n)
+        {
+            // Your awesome code here!
+            while (n > 9)
+            {
+                int tmp = 0;
+                var n_str = n.ToString();
+                foreach (var num in n_str)
+                {
+                    tmp += Convert.ToInt32(num) - '0';
+                }
+                n = Convert.ToInt64(tmp);
+            }
+            return Convert.ToInt32(n);
+        }
+    }
+}
---
+++ SumOfDigitsTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sum_of_Digits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Digits.Tests
+{
+    [TestClass()]
+    public class NumberTests
+    {
+        [TestMethod()]
+        public void DigitalRootTest()
+        {
+            var num = new Number();
+            // test one digit
+            Assert.AreEqual(9, num.DigitalRoot(9));
+            // test many digits
+            Assert.AreEqual(7, num.DigitalRoot(16));
+            // test many digits sum over 9
+            Assert.AreEqual(6, num.DigitalRoot(456));
+            // test int max
+            Assert.AreEqual(1, num.DigitalRoot(2147483647));
+            // test long
+            Assert.AreEqual(3, num.DigitalRoot(3333333333));
+            // test long max
+            Assert.AreEqual(7, num.DigitalRoot(9223372036854775807));
+        }
+    }
+}
